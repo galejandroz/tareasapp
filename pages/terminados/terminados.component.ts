aff8193cebbd2de4488edbf2eff94700834f427d
galejandroz <galejandroz@hotmail.com>
2018-12-19 22:40:21 -0300

    Add files via upload

---
+++ pages/terminados/terminados.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { DeseosService } from "../../services/deseos.services";
+import { Lista } from "../../models";
+
+
+
+@Component({
+  selector: 'page-terminados',
+  templateUrl: 'terminados.component.html'
+})
+
+export class TerminadosPage {
+
+  constructor( public deseosService:DeseosService ){
+
+  }
+
+  listaSeleccionada( lista: Lista ){
+    console.log(lista);
+  }
+
+}
